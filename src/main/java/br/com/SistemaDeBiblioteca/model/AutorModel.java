68c85ec9f6d857b11d47b3dcb368a0239d3227e4
Thiago Jose <thiago.wilton2704@gmail.com>
2024-10-09 15:51:33 -0300

    - Renomeando classe Autor para AutorModel. - Define o campo `id` como chave primária.

--- src/main/java/br/com/SistemaDeBiblioteca/model/AutorModel.java
+++ src/main/java/br/com/SistemaDeBiblioteca/model/AutorModel.java
@@ -1,9 +1,6 @@
 package br.com.SistemaDeBiblioteca.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
@@ -12,17 +9,18 @@ import javax.validation.constraints.NotNull;
 @Entity
 @Table(name = "autor")
 @Data
-public class Autor {
+public class AutorModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
     private Long id;
 
     @NotNull(message = "O nome não pode ser nulo")
     @NotEmpty(message = "O nome não pode ser vazio")
     private String nome;
 
-    public Autor() {}
+    public AutorModel() {}
 
-    public Autor(String nome) {
+    public AutorModel(String nome) {
         this.nome = nome;
     }
 }
