27e9af4befdf280f997cf8d96abc720275eecc5e
Thiago Jose <thiago.wilton2704@gmail.com>
2024-10-10 15:08:28 -0300

    Adiciona mapeamento One-to-Many entre AutorModel e LivroModel, com cascading e métodos equals, hashCode e toString.

--- src/main/java/br/com/SistemaDeBiblioteca/model/AutorModel.java
+++ src/main/java/br/com/SistemaDeBiblioteca/model/AutorModel.java
@@ -5,6 +5,9 @@ import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "autor")
@@ -18,9 +21,34 @@ public class AutorModel {
     @NotEmpty(message = "O nome não pode ser vazio")
     private String nome;
 
+    // Um autor pode ter muitos livros
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
+    private Set<LivroModel> livros;
+
     public AutorModel() {}
 
     public AutorModel(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AutorModel that = (AutorModel) o;
+        return Objects.equals(id, that.id); // Comparar apenas pelo ID
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id); // Usar apenas o ID para o hash
+    }
+
+
+    @Override
+    public String toString() {
+        return "AutorModel{" +
+                "nome='" + nome + '\'' +
+                '}';
     }
 }
