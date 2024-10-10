7238b2166c37bfb222f7ef97a9ce849c4be680d5
Thiago Jose <thiago.wilton2704@gmail.com>
2024-10-10 15:05:03 -0300

    Adiciona relações entre LivroModel, AutorModel e CategoriaModel
    - Implementa a relação `@ManyToOne` entre `LivroModel` e `AutorModel`, utilizando a anotação `@JoinColumn` para mapear a chave estrangeira `autor_id`.
    - Adiciona a relação `@ManyToMany` entre `LivroModel` e `CategoriaModel`, configurando a tabela intermediária `livro_categoria` com as colunas `livro_id` e `categoria_id`.
    - Implementa os métodos `equals` e `hashCode` para garantir comparações adequadas entre objetos `LivroModel`, baseando-se no ID.
    - Sobrescreve o método `toString` para retornar uma representação de `LivroModel`, exibindo os atributos `titulo` e `isbn`.

--- src/main/java/br/com/SistemaDeBiblioteca/model/LivroModel.java
+++ src/main/java/br/com/SistemaDeBiblioteca/model/LivroModel.java
@@ -5,6 +5,8 @@ import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "livro")
@@ -22,5 +24,39 @@ public class LivroModel {
     @NotEmpty(message = "O código de identificação do livro não pode ser vazio")
     private String isbn;
 
+    // Muitos livros podem ser escritos por um único autor
+    @ManyToOne
+    @JoinColumn(name = "autor_id", nullable = false)
+    private AutorModel autor;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "livro_categoria",
+            joinColumns = @JoinColumn(name = "livro_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private Set<CategoriaModel> categorias;
     public LivroModel() {}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true; // Se for o mesmo objeto
+        if (o == null || getClass() != o.getClass()) return false; // Verifica se é nulo ou se é de outra classe
+        LivroModel that = (LivroModel) o; // Converte para LivroModel
+        return Objects.equals(id, that.id); // Compara apenas pelo ID
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+    @Override
+    public String toString() {
+        return "LivroModel{" +
+                "titulo='" + titulo + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
